53d1d5bc5d3cbf1544d115f653b4fb972fe153a1
upbit <rmusique@gmail.com>
2013-06-23 22:49:24 +0800

    upload source

---
+++ demo.py
@@ -0,0 +1,65 @@
+# -*- coding:utf-8 -*- #
+#! /usr/bin/env python
+
+import time
+from oauth import OAuth2Handler
+from tweibo import API
+
+# 换成你的 APPKEY
+APP_KEY = "801376349"
+APP_SECRET = "c057534d14cba920579f2d54005e0c06"
+CALLBACK_URL = "https://github.com"
+# 请先按照 access_token_test() 说明填写 ACCESS_TOKEN 和 OPENID
+ACCESS_TOKEN = ""
+OPENID = ""
+IMG_EXAMPLE = "example.png"
+
+def access_token_test():
+    """ 访问get_access_token_url()的URL并授权后，会跳转callback页面，其中包含如下参数：
+        #access_token=00000000000ACCESSTOKEN0000000000&expires_in=8035200
+        &openid=0000000000000OPENID0000000000000&openkey=0000000000000OPENKEY000000000000
+        &refresh_token=0000000000REFRESHTOKEN00000000&state=
+    保存下其中的 access_token, openid 并调用
+        oauth.set_access_token(access_token)
+        oauth.set_openid(openid)
+    即可完成 OAuth2Handler() 的初始化。可以记录 access_token 等信息
+    """
+    oauth = OAuth2Handler()
+    oauth.set_app_key_secret(APP_KEY, APP_SECRET, CALLBACK_URL)
+    print oauth.get_access_token_url()
+
+def tweibo_test():
+    oauth = OAuth2Handler()
+    oauth.set_app_key_secret(APP_KEY, APP_SECRET, CALLBACK_URL)
+    oauth.set_access_token(ACCESS_TOKEN)
+    oauth.set_openid(OPENID)
+
+    api = API(oauth)
+    #api = API(oauth, host="127.0.0.1", port=8888)       # Init API() with proxy
+
+    # GET /t/show
+    #tweet1 = api.get.t__show(format="json", id=301041004850688)
+    #print ">> %s: %s" % (tweet1.data.nick, tweet1.data.text)
+
+    # POST /t/add
+    #content_str = "[from PySDK] %s says: %s" % (tweet1.data.nick, tweet1.data.origtext)
+    #tweet2 = api.post.t__add(format="json", content=content_str, clientip="10.0.0.1")
+    #print ">> time=%s, http://t.qq.com/p/t/%s" % (tweet2.data.time, tweet2.data.id)
+
+    # GET /statuses/user_timeline
+    #user_timeline = api.get.statuses__user_timeline(format="json", name="qqfarm", reqnum=3, pageflag=0, lastid=0, pagetime=0, type=3, contenttype=0)
+    #for idx, tweet in enumerate(user_timeline.data.info):
+    #    print "[%d] http://t.qq.com/p/t/%s, (type:%d) %s" % (idx+1, tweet.id, tweet.type, tweet.text)
+
+    # UPLOAD /t/upload_pic
+    pic1 = api.upload.t__upload_pic(format="json", pic_type=2, pic=open(IMG_EXAMPLE, "rb"))
+    print ">> IMG: %s" % (pic1.data.imgurl)
+
+    # POST /t/add_pic_url
+    content_str2 = "[from PySDK] add pic demo: %s, time %s" % (IMG_EXAMPLE, time.time())
+    pic_urls = "%s" % (pic1.data.imgurl)
+    tweet_pic1 = api.post.t__add_pic_url(format="json", content=content_str2, pic_url=pic_urls, clientip="10.0.0.1")
+    print ">> time=%s, http://t.qq.com/p/t/%s" % (tweet_pic1.data.time, tweet_pic1.data.id)
+
+if __name__ == '__main__':
+    tweibo_test()
